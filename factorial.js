b48987e867a3cbf7b32ac0ab3aa7c4a1805560c6
Mac <mazharulislame.cse@gmail.com>
2023-08-07 21:45:51 +0600

    done

---
+++ factorial.js
@@ -0,0 +1,9 @@
+function factorial(number){
+    for(let i = 1; i <= number; i++){
+        result = result * 1;
+    }
+    return result;
+}
+
+var multipication = 9;
+console.log(factorial(6));
--- findoddsum.js
+++ findoddsum.js
@@ -1,10 +1,26 @@
 function getSumOfAnArray(array){
+    let sum = 0;
     for(let i = 0; i < array.length; i++){
         const index = i;
         const element = array[index];
-        console.log(index, element);
+        sum = sum + element;
+        // console.log(index, element, sum);
     }
+    return sum;
+}
+function getSumOfOdd(array){
+    const oddArray = [];
+    for(let i = 0; i < array.length; i++){
+        const index = i;
+        const element = array[index];
+        if(element % 2 !== 0){
+            console.log(index, element);
+            oddArray.push(element);
+        }
+    } 
+    return oddArray;   
 }
 
 const myArray = [12, 23, 34, 45, 56, 67, 78, 89, 90];
 getSumOfAnArray(myArray);
+const oddNumberSum = getSumOfAnArray
